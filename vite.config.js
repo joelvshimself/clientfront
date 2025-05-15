875d5fcd1fd889e088583c31607d5505a8c20db4
Joel Vargas Reynoso <76189923+joelvshimself@users.noreply.github.com>
2025-05-14 21:53:04 -0600

    added vitest
    just one test for logo rendering

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import process from 'node:process' 
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import process from 'node:process';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '');
 
-  console.log("API URL usada por Vite:", env.VITE_API_URL)
+  console.log('API URL usada por Vite:', env.VITE_API_URL);
 
   return {
     plugins: [react()],
     define: {
-      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)
-    }
-  }
-})
+      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
+    },
+    test: {
+      environment: 'jsdom',
+      setupFiles: './vitest.setup.js',
+    },
+  };
+});
