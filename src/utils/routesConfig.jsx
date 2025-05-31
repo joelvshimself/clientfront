86409660816fd943f90901137e41ce3ddeaab632
Adrian Guereque <aguereque96@gmail.com>
2025-05-31 12:06:54 -0600

    Add profile route

--- src/utils/routesConfig.jsx
+++ src/utils/routesConfig.jsx
@@ -1,3 +1,4 @@
+import { element } from "prop-types";
 import { lazy } from "react";
 
 const Home = lazy(() => import("../pages/home"));
@@ -9,21 +10,24 @@ const ConfirmarOrden = lazy(() => import("../pages/orden/ConfirmarOrden"));
 const Venta = lazy(() => import("../pages/venta/venta"));
 const SeleccionVenta = lazy(() => import("../pages/venta/SeleccionVenta"));
 const ConfirmarVenta = lazy(() => import("../pages/venta/ConfirmarVenta"));
+const PerfilUsuario = lazy(() => import("../pages/PerfilUsuario"))
 
 export const protectedRoutes = [
-  { path: "/home", element: <Home />, roles: ["admin", "owner"] },
-  { path: "/usuarios", element: <Usuarios />, roles: ["admin", "detallista", "owner"] },
+  { path: "/home", element: <Home />, roles: ["owner", "detallista"] },
+  { path: "/usuarios", element: <Usuarios />, roles: ["admin"] },
   { path: "/orden", element: <Ordenes />, roles: ["detallista", "proveedor"] },
   { path: "/venta", element: <Venta />, roles: ["detallista"] },
   { path: "/orden/nueva/proveedor", element: <SeleccionProveedor />, roles: ["detallista"] },
   { path: "/orden/nueva/producto", element: <SeleccionProducto />, roles: ["detallista"] },
-  { path: "/orden/nueva/confirmar", element: <ConfirmarOrden />, roles: ["detallista"] },
+  { path: "/orden/nueva/confirmar", element: <ConfirmarOrden />, roles: ["proveedor"] },
   { path: "/venta/nueva", element: <SeleccionVenta />, roles: ["detallista"] },
   { path: "/venta/nueva/confirmar", element: <ConfirmarVenta />, roles: ["detallista"] },
+  { path: "/profile", element: <PerfilUsuario />, roles: ["owner", "detallista", "admin", "proveedor"]}
 ];
 
 export const roleToPath = {
-  admin: "/home",
-  detallista: "/orden",
-  proveedor: "/orden"
+  admin: "/usuarios",
+  detallista: "/home",
+  proveedor: "/orden",
+  owner: "/home"
 };
