bb0b295c1c1e933ae59dda8e350ed411d2ba98e4
Adrian Guereque <aguereque96@gmail.com>
2025-05-31 01:01:04 -0600

    Fix: Load Vite Url from .env for future prod release

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import process from 'node:process';
 
 export default defineConfig(({ mode }) => {
-  loadEnv(mode, process.cwd(), ''); // aún se puede cargar .env por si se usa en otros lados
+  const env = loadEnv(mode, process.cwd(), '');
 
-  console.log("API URL usada por Vite:", "http://localhost:3000/api");
+  console.log("API URL usada por Vite:", env.VITE_API_URL);
 
   return {
     plugins: [react()],
     define: {
-      'import.meta.env.VITE_API_URL': JSON.stringify("http://localhost:3000/api"),
+      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
     },
   };
 });
