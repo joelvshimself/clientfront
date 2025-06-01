90c68e3bdab19167585ab74bb46eff52b7edb82f
Adrian Guereque <aguereque96@gmail.com>
2025-05-31 18:51:25 -0600

    Add developer role

--- src/utils/routesConfig.jsx
+++ src/utils/routesConfig.jsx
@@ -13,21 +13,22 @@ const ConfirmarVenta = lazy(() => import("../pages/venta/ConfirmarVenta"));
 const PerfilUsuario = lazy(() => import("../pages/PerfilUsuario"))
 
 export const protectedRoutes = [
-  { path: "/home", element: <Home />, roles: ["owner", "detallista"] },
-  { path: "/usuarios", element: <Usuarios />, roles: ["admin"] },
-  { path: "/orden", element: <Ordenes />, roles: ["detallista", "proveedor"] },
-  { path: "/venta", element: <Venta />, roles: ["detallista"] },
-  { path: "/orden/nueva/proveedor", element: <SeleccionProveedor />, roles: ["detallista"] },
-  { path: "/orden/nueva/producto", element: <SeleccionProducto />, roles: ["detallista"] },
-  { path: "/orden/nueva/confirmar", element: <ConfirmarOrden />, roles: ["proveedor"] },
-  { path: "/venta/nueva", element: <SeleccionVenta />, roles: ["detallista"] },
-  { path: "/venta/nueva/confirmar", element: <ConfirmarVenta />, roles: ["detallista"] },
-  { path: "/profile", element: <PerfilUsuario />, roles: ["owner", "detallista", "admin", "proveedor"]}
+  { path: "/home", element: <Home />, roles: ["owner", "detallista","developer"] },
+  { path: "/usuarios", element: <Usuarios />, roles: ["admin","developer"] },
+  { path: "/orden", element: <Ordenes />, roles: ["detallista", "proveedor","developer"] },
+  { path: "/venta", element: <Venta />, roles: ["detallista","developer"] },
+  { path: "/orden/nueva/proveedor", element: <SeleccionProveedor />, roles: ["detallista","developer"] },
+  { path: "/orden/nueva/producto", element: <SeleccionProducto />, roles: ["detallista","developer"] },
+  { path: "/orden/nueva/confirmar", element: <ConfirmarOrden />, roles: ["detallista","developer"] },
+  { path: "/venta/nueva", element: <SeleccionVenta />, roles: ["detallista","developer"] },
+  { path: "/venta/nueva/confirmar", element: <ConfirmarVenta />, roles: ["detallista","developer"] },
+  { path: "/profile", element: <PerfilUsuario />, roles: ["owner", "detallista", "admin", "proveedor","developer"]}
 ];
 
 export const roleToPath = {
   admin: "/usuarios",
   detallista: "/home",
   proveedor: "/orden",
-  owner: "/home"
+  owner: "/home",
+  developer: "/home"
 };
