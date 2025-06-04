7fe202ba3004df91f68fb83843b25425f946d768
Adrian Guereque <aguereque96@gmail.com>
2025-06-03 18:22:55 -0600

    Fix: Remove debugging logs

--- src/utils/protectedRoute.jsx
+++ src/utils/protectedRoute.jsx
@@ -4,7 +4,7 @@ import NotFound from "../pages/NotFound"
 
 export const ProtectedRoute = ({ allowedRoles, children }) => {
   const { user, loading } = useAuth();
-  console.log(user)
+  //console.log(user)
   if (loading) return <p>Cargando...</p>;
   if (!user) return <Navigate to="/login" />;
   if(!user.twoFa) return <Navigate to="/2fa" />;
